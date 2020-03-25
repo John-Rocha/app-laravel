3b5fee51f2a69a3eb11502358b8a7f829602cff4
JohnRocha <johnathanrocha@gmail.com>
2020-03-24 23:34:15 -0300

    Laravel

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        factory(User::class, 10)->create();
+        /*
+        User::create([
+            'name' => 'John Rocha',
+            'email' => 'johnathanrocha@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+        */
+    }
+}
